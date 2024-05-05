10c0252611763cc4f394dd12470dd7417e931168
MahinAshraful <150500453+MahinAshraful@users.noreply.github.com>
2024-05-05 19:39:16 -0400

    added cat me and you

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,38 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
+import './App.css';
 
 function App() {
   const [dogImageUrl, setDogImageUrl] = useState(null);
+  const [catImageUrl, setCatImageUrl] = useState(null);
 
   useEffect(() => {
-    const fetchDogImage = async () => {
-      const response = await fetch('https://dog.ceo/api/breeds/image/random');
-      const data = await response.json();
-      setDogImageUrl(data.message);
+    const fetchImages = async () => {
+      const dogResponse = await fetch('https://dog.ceo/api/breeds/image/random');
+      const dogData = await dogResponse.json();
+      setDogImageUrl(dogData.message);
+
+
+      const catResponse = await fetch('https://api.thecatapi.com/v1/images/search?limit=1');
+      const catData = await catResponse.json();
+      setCatImageUrl(catData[0].url);
     };
 
-    fetchDogImage();
+    fetchImages();
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        {dogImageUrl && <img src={dogImageUrl} alt="Random Dog" />}
-        <br />
-        <button onClick={() => window.location.reload()}>Refresh for New Dog</button>
-        <p>Mahin loves Ashleigh</p>
+        <div className="dog-container">
+        <p className="text">Me</p>
+          <img src={dogImageUrl} alt="Random Dog" className="pet-image" />
+          
+        </div>
+        <div className="cat-container">
+        <p className="text">You</p>
+          <img src={catImageUrl} alt="Random Cat" className="pet-image" />
+        </div>
+        <button onClick={() => window.location.reload()}>Refresh for New Pets</button>
       </header>
     </div>
   );
